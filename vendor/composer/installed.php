79d9dc2b63aa50b1a41d511d0843f04be5d552b3
Scott Weaver <sweaver@sharethis.com>
2022-06-15 00:58:07 -0700

    fix: PP-3101 Include composer vendor files in plugin

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'sharethis/share-buttons',
+  ),
+  'versions' => 
+  array (
+    'sharethis/share-buttons' => 
+    array (
+      'pretty_version' => '1.0.0',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
